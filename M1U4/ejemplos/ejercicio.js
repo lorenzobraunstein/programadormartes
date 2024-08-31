b032da32d12fa5570e8f2551aeae17a18c1739df
Lorenzo Braunstein <lorenzobraunstein@gmail.com>
2024-08-31 20:44:15 -0300

    ejerciciosjs

--- M1U4/ejemplos/ejercicio.js
+++ M1U4/ejemplos/ejercicio.js
@@ -1,1 +1,21 @@
-var num = 10;
+var distance; //nose como hacer un random.range en js :/
+var transport;
+
+void travel(); {
+    if (distance < 1000) {
+        transport = "Pie";
+    }
+    else if (distance > 1000 && distance < 10000) {
+        transport = "Bicicleta";
+    }
+    else if (distance > 10000 && distance < 30000) {
+        transport = "Colectivo";
+    }
+    else if (distance > 30000 && distance < 100000) {
+        transport = "Auto";
+    }
+    else {
+        transport = "Avion";
+    }
+
+}
---
+++ M1U4/ejercicio2.js
@@ -0,0 +1,11 @@
+void Main()
+{
+    const myArray = [12, 21, 4, 86, 37, 734, 1];
+    const maxNum = 0;
+    for (let f = 1; f <= myArray.length; f++) {
+        if(maxNum < myArray[i]){
+            maxNum = myArray[i];
+        }
+    }
+    console.log(maxNum);
+}
